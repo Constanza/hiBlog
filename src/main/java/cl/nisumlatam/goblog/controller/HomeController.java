f25bd8564954a2163311d39fb1b660f78b700875
Alexander Castillo <alcastic.dev@gmail.com>
2018-01-23 11:56:19 -0300

    [@alcastic] add spring web conf. add basic home controller.

---
+++ src/main/java/cl/nisumlatam/goblog/controller/HomeController.java
@@ -0,0 +1,13 @@
+package cl.nisumlatam.goblog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Be original!!";
+    }
+}
