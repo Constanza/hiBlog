b60a4f0a2a220853280087dcf6146bcb97d33651
Alexander Castillo <alcastic.dev@gmail.com>
2018-01-23 11:24:26 -0300

    [@alcastic] Initial coomit. Add SpringBott conf.

---
+++ src/main/java/cl/nisumlatam/goblog/GoblogApplication.java
@@ -0,0 +1,12 @@
+package cl.nisumlatam.goblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoblogApplication.class, args);
+	}
+}
